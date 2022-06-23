8ba2cc15b62f104400a6f243ae4f51ea05f84d9a
MarianneBricquir <marianne.bricquir@gmail.com>
2022-06-23 07:57:54 +0200

    page product (sauf liste choix et panier)

--- front/js/index.js
+++ front/js/index.js
@@ -1,21 +1,31 @@
-let url = `http://localhost:3000/api/products`;
+// fonction qui permet d'afficher les produits sur la page d'accueil de façon dynamique
+// Paramètre : url de l'api
+function listProduct (url) {
+    fetch(url)
+        .then((response) =>
+            response.json()
+                .then((data => {
+                    let affichage = '';
+                            for (let product of data) {
+                                affichage += `<a href="./product.html?id=${product._id}">
+                                <article>
+                                  <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
+                                  <h3 class="productName">${product.name}</h3>
+                                  <p class="productDescription">${product.description}</p>
+                                </article>
+                                </a>`
+                            }
+                            document.querySelector("#items").innerHTML = affichage;
+                })))
+        .catch(error => console.log(`Erreur : ` + err));
+}
 
-fetch(url)
-    .then((response) =>
-        response.json()
-            .then((data => {
-                console.log(data);
-                let affichage = '';
-                        for (let product of data) {
-                            affichage += `<a href="./product.html?${product._id}">
-                            <article>
-                              <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
-                              <h3 class="productName">${product.name}</h3>
-                              <p class="productDescription">${product.description}</p>
-                            </article>
-                            </a>
-                          `
-                        }
-                        document.querySelector("#items").innerHTML = affichage;
-            })))
-    .catch(error => console.log(`Erreur : ` + err));
+// Appel de la fonction
+listProduct(`http://localhost:3000/api/products`);
+
+
+
+
+
+
+
---
+++ front/js/product.js
@@ -0,0 +1,50 @@
+// Page produit
+// Fonction qui récupère l'id de l'url d'une page produit
+// paramètre : url de la page
+function addId(urlPage) {
+    let search_params = new URLSearchParams(urlPage.search);
+    if (search_params.has('id')) {
+        let id = search_params.get('id');
+        return id
+    };
+};
+
+let currentUrl = window.location; // méthode qui permet de récupérer l'url de la page actuelle
+let currentId = addId(currentUrl); // on stocke le résultat de la fonction dans la variable currentId
+//console.log(currentId) // pour vérifier que l'on récupère bien l'id
+
+
+// Fonction : appel à l'API pour récupérer les informations et les transcrire dans le html
+// paramètre de la fonction : url de l'api et l'id du produit récupéré
+function infoProduct(urlWithId) {
+    fetch(urlWithId)
+        .then((response) =>
+            response.json()
+                .then(data => {
+                    console.log(data);
+                    document.querySelector('title').textContent = `${data.name}`;
+                    document.querySelector('.item__img').innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}, ${data.name}">`;
+                    document.querySelector('#title').textContent = `${data.name}`;
+                    document.querySelector('#price').textContent = `${data.price}`;
+                    document.querySelector('#description').textContent = `${data.description}`;
+                    let colorOptions = data.colors ;
+                    let colorLength = colorOptions.length;
+                    console.log(colorOptions);
+                    let option = `<option `;
+                     /*for (let c = 0; c < colorLength; c++) {
+                        let choose = colorOptions[c];
+                        if{
+
+                        }else{
+
+                        }
+                        option += document.querySelector(${option}).innerHTML = `${option} value=${choose}>${choose}`;
+                     }
+                    option += `</option>`*/
+                }));
+}
+
+
+// Appel de la fonction dans la page produit
+let currentUrlWithId = `http://localhost:3000/api/products/${currentId}`
+infoProduct(currentUrlWithId)
